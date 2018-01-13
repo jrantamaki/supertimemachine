84a483d6a50c43effb1b0d14591e897d2f4ab1c5
Mikko Ravimo <mrav@FL911.home>
2018-01-13 20:33:16 +0200

    Add elm files to gitignore

--- elm-client/Main.elm
+++ elm-client/Main.elm
@@ -4,7 +4,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-view = h1 [] [text "welcome to The SuperTimeMachine"]
+view = h1 [] [text "Welcome to The SuperTimeMachine (powered by Elm)"]
 
 main = view
 
