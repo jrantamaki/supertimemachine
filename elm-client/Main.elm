fc8d4427defb07a5cc3b55919bc988385a12f8f2
Mikko Ravimo <mrav@FL911.home>
2018-01-14 16:41:40 +0200

    TEMP

--- elm-client/Main.elm
+++ elm-client/Main.elm
@@ -10,21 +10,31 @@ import Model exposing(Task)
 -- Model
 type alias ModelType = { currentTask : Task }
 model : ModelType
-model = { currentTask = Task "Some description" }
+model = { currentTask = Task "Not fetched yet" }
 
 -- View
 view : ModelType -> Html MsgType
 view model =
     div []
-        [ button [ onClick Increment ] [ text "-" ]
+        [ button [ onClick FetchTaskCommand ] [ text "-" ]
+        , br
+        , text "Current task: "
         , text model.currentTask.description
         ]
 
 -- Update
-type MsgType = Increment
+type MsgType = FetchTaskCommand
+    | FetchTaskResult Task
+
+
+fetchTask : FetchTaskResult
+fetchTask = FetchTaskResult Task "Fetched task!"
 
 update : MsgType -> ModelType -> ModelType
-update msg model = ModelType (Task "new description")
+update msg model =
+    case msg of FetchTaskCommand -> ModelType fetchTask
+    case msg of FetchTaskResult ->
+        ModelType (Task "new descriptio")
 
 
 -- Entry
