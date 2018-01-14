95cd1eec066832b34da9d1437f2e9e481e68dd2d
Mikko Ravimo <mrav@FL911.home>
2018-01-14 15:41:59 +0200

    TEMP

--- elm-client/Main.elm
+++ elm-client/Main.elm
@@ -3,8 +3,30 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
-view = h1 [] [text "Welcome to The SuperTimeMachine (powered by Elm)"]
 
-main = view
+type alias Model = Int
+myModel : Model
+myModel = 1
+
+type Msg = Increment
+
+
+myView : Model -> Html Msg
+myView model =
+    div []
+        [ button [ onClick Increment ] [ text "-" ]
+        , text (toString model)
+        ]
+
+myUpdate : Msg -> Model -> Model
+myUpdate msg model = model + 1
+
+main =
+    beginnerProgram {
+        model = myModel,
+        view = myView,
+        update = myUpdate
+    }
 
