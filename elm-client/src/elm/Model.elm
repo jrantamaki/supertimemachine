acb480e1b1a587ca7e8bd9172b77a658d7663109
mravimo <mikko.ravimo@gmail.com>
2018-02-10 10:52:34 +0200

    New task submit for UI

--- elm-client/src/elm/Model.elm
+++ elm-client/src/elm/Model.elm
@@ -2,12 +2,30 @@ module Model exposing (..)
 
 import Date exposing (Date)
 import Time exposing (..)
+import Http
+
+-- ******************
+-- **** Messages ****
+-- ******************
+type MsgType =
+    -- Fired from UI to fetch a task
+    FetchTaskCommand Config
+    -- Message for fetching task result that carries the fetched task
+    | FetchTaskResult (Result Http.Error TaskList)
+    -- Fired from UI to post new task
+    | SubmitTaskCommand NewTaskForm
+    -- Result of POST task API call
+    | SubmitTaskResult (Result Http.Error TaskEntry)
+    -- To update current time
+    | Tick Time
+    | NewTaskFormInput InputMessage
+
+type InputMessage = OnTagInput String | OnDescInput String
 
 -- ***************
 -- **** Model ****
 -- ***************
 type alias Config =  { apiUrl : String }
-
 
 type alias TaskEntry = {
     description : String,
@@ -17,10 +35,16 @@ type alias TaskEntry = {
 
 type alias TaskList = { tasks : List TaskEntry }
 
+type alias NewTaskForm = {
+    descInput: String,
+    tagInput: String,
+    tags: List String }
+
 type alias ModelType = {
     taskList : TaskList,
     config : Config,
     error: String,
-    timeNow: Time }
+    timeNow: Time,
+    newTaskForm: NewTaskForm }
 
 
--- elm-client/src/elm/View.elm
+++ elm-client/src/elm/View.elm
@@ -1,9 +1,10 @@
-module View exposing (taskListView)
+module View exposing (taskListView, newTaskFormView)
 
 import Date exposing (Date)
 import Model exposing(..)
 import Html exposing (..)
-import Html.Attributes exposing (class,id,hidden)
+import Html.Attributes exposing (class,id,hidden, placeholder,value)
+import Html.Events exposing (onInput,onClick)
 import Date.Extra.Format as Format exposing (format, formatUtc, isoMsecOffsetFormat)
 import Date.Extra.Config.Config_en_us exposing (config)
 import Date.Extra.Period as Period
@@ -20,17 +21,24 @@ taskView task timeNow =
     ul [ class "list-group list-group-flush"] [
         li [class "list-group-item"] [ text task.description],
         li [class "list-group-item"] [ span [] [ text (formatDate task.startedAt)]],
-        li [class "list-group-item"] [ span [] [ text (printStopTime task.stoppedAt)]],
+        li [class "list-group-item"] [ span [] [ text (formatMaybeDate task.stoppedAt)]],
         li [class "list-group-item"] [ text (printDuration task.startedAt task.stoppedAt timeNow)],
-        li [class "list-group-item"] (List.map (\s -> span [] [ span [ class "badge badge-primary" ] [text s], text " "]) task.tags)
+        li [class "list-group-item"] (renderTags task.tags)
     ]
 
+newTaskFormView : NewTaskForm -> Html MsgType
+newTaskFormView taskForm =
+    div [] [
+        input [ placeholder "Description", onInput (NewTaskFormInput << OnDescInput)] [],
+        div [] [ span [] (renderTags taskForm.tags) ],
+        input [ placeholder "tags", value taskForm.tagInput, onInput (NewTaskFormInput << OnTagInput)] [],
+        div [ ] [ button [class "btn btn-primary", onClick (SubmitTaskCommand taskForm)] [ text "Start" ] ]
+    ]
 
-printStopTime : Maybe Date -> String
-printStopTime maybe =
-    case maybe of
-        Nothing -> ""
-        Just val -> formatDate val
+renderTags : List String -> List (Html msg)
+renderTags tasks =
+    (List.map (\s -> span [] [ span [ class "badge badge-primary" ] [text s], text " "]) tasks)
+
 
 printDuration : Date -> Maybe Date -> Time -> String
 printDuration start stop timeNow =
@@ -38,9 +46,16 @@ printDuration start stop timeNow =
         period = Period.diff (Maybe.withDefault (Date.fromTime timeNow) stop) start
         duration = Duration.diff (Maybe.withDefault (Date.fromTime timeNow) stop) start
     in
-    toString (( (period.week * 7 + period.day) * 24) + duration.hour) ++ "h " ++
-    toString duration.minute ++ "m " ++
-    toString duration.second ++ "s"
+        toString (( (period.week * 7 + period.day) * 24) + duration.hour) ++ "h " ++
+        toString duration.minute ++ "m " ++
+        toString duration.second ++ "s"
 
 formatDate : Date -> String
 formatDate d = Format.format config "%d.%m.%Y %H:%M:%S" d
+
+formatMaybeDate : Maybe Date -> String
+formatMaybeDate maybe =
+    case maybe of
+        Nothing -> ""
+        Just val -> formatDate val
+
