2253b51a7356f74a8498b052d301a60d3ca1ffd3
mravimo <mikko.ravimo@gmail.com>
2018-02-16 21:11:41 +0200

    Use Time data type for the timestamps

--- backend/src/supertimemachine/model/task.go
+++ backend/src/supertimemachine/model/task.go
@@ -1,13 +1,16 @@
 package model
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
 
 type Task struct {
 	Id          	bson.ObjectId		`bson:"_id" json:"id"`
 	Description 	string    		`json:"description"`
 	Tags        	[]string		`json:"tags"`
-	Started_at	string			`json:"started_at"`
-	Stopped_at	string			`json:"stopped_at,omitempty"`
+	Started_at	time.Time		`json:"started_at"`
+	Stopped_at	*time.Time		`json:"stopped_at,omitempty"` // Having as pointer to make nullable
 }
 
 type TaskList struct {
