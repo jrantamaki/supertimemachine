fc67ae2030f5b92a53e7e054b9251538c83557ec
mravimo <mikko.ravimo@gmail.com>
2018-04-01 19:19:05 +0300

    TEMP

--- elm-client/src/elm/Tokens.elm
+++ elm-client/src/elm/Tokens.elm
@@ -1,16 +1,29 @@
 module Tokens exposing(..)
 
-type alias CognitoToken = {
-    sub: String,
-    userName: String }
+import Jwt exposing(..)
+import Jwt.Decoders exposing (JwtToken)
+import Json.Decode as Json exposing (Decoder, field)
 
-
+--type alias ExtendedToken a = { a | userName: String }
+--type alias CognitoToken = ExtendedToken(JwtToken)
 
 userName : String -> String
 userName token =
-    (decode token).userName
+    decode token |> Result.map .userId |> Result.map (Maybe.withDefault "") |> Result.withDefault ""
 
-decode : String -> CognitoToken
-
+decode : String -> Result JwtError JwtToken
 decode token =
-    CognitoToken "subject" "userName"
+    decodeToken coqnitoDecoder token
+
+coqnitoDecoder : Decoder JwtToken
+coqnitoDecoder =
+    Json.succeed JwtToken
+        |> andMap (field "iat" Json.int) -- JwtToken.iat
+        |> andMap (field "exp" Json.int) -- JwtToken.exp
+        |> andMap (Json.maybe <| field "sub" Json.string) -- JwtToken.userId
+        |> andMap (Json.maybe <| field "email" Json.string) -- JwtToken.email
+--        |> andMap (field "coqnito:username" Json.string) -- CognitoToken.userName
+
+andMap : Decoder a -> Decoder (a -> b) -> Decoder b
+andMap =
+    Json.map2 (|>)
